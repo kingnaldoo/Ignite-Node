418e04c32b8e3978864c0963c69886de7958a5b7
kingnaldoo <reinaldosn2016@gmail.com>
2021-09-20 21:29:29 -0300

    add typescript and ts-node-dev

---
+++ Chapter 2 - Iniciando a API/rentalx/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({message: 'Hello World'})
+})
+
+app.listen(3000, () => console.log('O servidor está rodando 😎'))
