edddae82c391f5b93d0952d5beff482b89ccdfbb
kingnaldoo <reinaldosn2016@gmail.com>
2021-09-21 11:08:32 -0300

    add eslint config

--- Chapter 2 - Iniciando a API/rentalx/src/server.ts
+++ Chapter 2 - Iniciando a API/rentalx/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({message: 'Hello World'})
-})
+	res.json({message: 'Hello World'});
+});  
 
-app.listen(3000, () => console.log('O servidor está rodando 😎'))
+app.listen(3000, () => console.log('O servidor está rodando 😎'));
