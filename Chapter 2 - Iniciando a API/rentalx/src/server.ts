d5b9f996536418d99581fdb7a8e84e0753c63f87
kingnaldoo <reinaldosn2016@gmail.com>
2021-10-09 11:02:31 -0300

    cache clean

---
+++ Chapter 2 - Iniciando a API/rentalx/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express(); 
+
+app.get('/', (req, res) => { 
+    res.json({message: "hello world"})
+})
+
+app.listen(3000, () => console.log('O servidor está rodando😎')); 
