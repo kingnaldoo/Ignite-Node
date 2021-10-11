f419b0010b0638f8e1b621e07c339a29076031bd
kingnaldoo <reinaldosn2016@gmail.com>
2021-10-11 14:53:11 -0300

    separate repositories

--- Chapter 2 - Iniciando a API/rentalx/src/modules/cars/repositories/implementations/SpecificationRepository.ts
+++ Chapter 2 - Iniciando a API/rentalx/src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -1,5 +1,5 @@
-import { Specification } from '../model/Specification';
-import { ISpecificationsRepository, ICreateSpecificationsDTO } from './ISpecificationsRepository';
+import { Specification } from '../../model/Specification';
+import { ICreateSpecificationsDTO, ISpecificationsRepository } from '../ISpecificationsRepository';
 
 class SpecificationsRepository implements ISpecificationsRepository {
     private specifications: Specification[];
